8ea44f18f8f807d1e4287ce3c69de08a21c025da
chasedavid <chasedavidfarnsworth@gmail.com>
2017-12-09 12:36:41 -0800

    normed, missing header

--- Fillit_SPARKY/sample.c
+++ Fillit_SPARKY/sample.c
@@ -2,14 +2,14 @@
 
 void	delimit(char *file_string, char c)
 {
-	int     i;
-	int     count;
+	int		i;
+	int		count;
 
 	i = 0;
 	count = 0;
 	while (file_string[i] != '\0')
 	{
-		if (file_string[i] == '\n' && file_string[i+1] == '\n')
+		if (file_string[i] == '\n' && file_string[i + 1] == '\n')
 		{
 			file_string[i + 1] = c;
 		}
@@ -17,54 +17,31 @@ void	delimit(char *file_string, char c)
 	}
 }
 
-int   check_hashes(char **file_string)
+int		check_hashes(char **file_string)
 {
-	int     i;
-	int     conn;
+	int		i;
+	int		conn;
 
 	i = 0;
 	conn = 0;
-	while((*file_string)[i] != '\0')
+	while ((*file_string)[i] != '\0')
 	{
 		if ((*file_string)[i] == '#')
 		{
 			if ((i + 1 >= 0) && (i + 1 <= (TET_SIZE)))
-			{
 				if ((*file_string)[i + 1] == '#')
-				{
 					conn++;
-				}
-			}
 			if ((i - 1 >= 0) && (i - 1 <= (TET_SIZE)))
-			{
 				if ((*file_string)[i - 1] == '#')
-				{
 					conn++;
-				}
-			}
 			if ((i + TET_WIDTH >= 0) && (i + 1 <= (TET_SIZE)))
-			{
-				if ((*file_string)[i + TET_WIDTH] == '#')
-				{
-					conn++;
-				}
-			}
+				conn += (((*file_string)[i + TET_WIDTH] == '#') ? 1 : 0);
 			if ((i - TET_WIDTH >= 0) && (i - 1 <= (TET_SIZE)))
-			{
-				if ((*file_string)[i - TET_WIDTH] == '#')
-				{
-					conn++;
-				}
-			}
+				conn += (((*file_string)[i - TET_WIDTH] == '#') ? 1 : 0);
 			i++;
 		}
 		else
-		{
 			i++;
-		}
 	}
-	if (conn == 8 || conn == 6)
-		return (1);
-	else
-		return (0);
+	return ((conn == 8 || conn == 6) ? 1 : 0);
 }
