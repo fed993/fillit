6cb4ba780d305a7d5fa7567518ca0f0f67bbaa87
chasedavid <chasedavidfarnsworth@gmail.com>
2017-12-09 12:14:22 -0800

    norming

--- Fillit_SPARKY/map.c
+++ Fillit_SPARKY/map.c
@@ -4,44 +4,41 @@
  * creating map, place final tetrimino on map, printing map, and freeing map
  */
 
- void    smallest_square(int nbr_of_pieces)
- {
-     int i;
-     int x;
-     int y;
-     char **smallest_square_array;
-
-     i = 0;
-     while ((i * i) < (nbr_of_pieces * 4))
-         i++;
-     smallest_square_array = (char **)malloc(sizeof(char *) * (i * i));
-     y = 0;
-     while (y < i)
-     {
-         smallest_square_array[y] = (char *)malloc(sizeof(char) * i);
-         y++;
-     }
-     y = 0;
-     while (y < i)
-     {
-         x = 0;
-         while (x < i)
-         {
-             smallest_square_array[y][x] = '.';
-             x++;
-         }
-         y++;
-     }
- }
-
-void print_board(char ***board_array)
+void	smallest_square(int nbr_of_pieces)
 {
-    int i;
+	int		i;
+	int		x;
+	int		y;
+	char	**smallest_square_array;
 
-    i = 0;
-    while ((i < 104) && ((*board_array)[i] != NULL))
-    {
-        ft_putstr((*board_array)[i]);
-        i++;
-    }
+	i = 0;
+	while ((i * i) < (nbr_of_pieces * 4))
+		i++;
+	smallest_square_array = (char **)malloc(sizeof(char *) * (i * i));
+	y = -1;
+	while (++y < i)
+		smallest_square_array[y] = (char *)malloc(sizeof(char) * i);
+	y = 0;
+	while (y < i)
+	{
+		x = 0;
+		while (x < i)
+		{
+			smallest_square_array[y][x] = '.';
+			x++;
+		}
+		y++;
+	}
+}
+
+void	print_board(char ***board_array)
+{
+	int i;
+
+	i = 0;
+	while ((i < 104) && ((*board_array)[i] != NULL))
+	{
+		ft_putstr((*board_array)[i]);
+		i++;
+	}
 }
